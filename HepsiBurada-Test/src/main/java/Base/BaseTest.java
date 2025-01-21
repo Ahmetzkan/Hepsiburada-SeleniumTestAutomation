f31380de5458f3efd80a344c359794d0dcaaf0f1
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2025-01-21 19:04:08 +0300

    Add (Postman Collection, Allure reports pictures, Tests pictures)

---
+++ HepsiBurada-Test/src/main/java/Base/BaseTest.java
@@ -0,0 +1,24 @@
+package Base;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest extends BaseLibrary {
+
+    @BeforeMethod(description = "")
+    public void openBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        driver = new ChromeDriver(options);
+
+        driver.get(loginUrl);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod(description = "The browser is closed.")
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
---
+++ HepsiBurada-Test/src/test/java/LoginTests.java
@@ -0,0 +1,36 @@
+import Base.BaseTest;
+import Pages.LoginPage;
+import org.testng.annotations.Test;
+
+public class LoginTests extends BaseTest {
+    LoginPage loginPage = new LoginPage();
+
+    @Test(description = "Successful login check")
+    public void LoginSuccesful() throws InterruptedException {
+        loginPage.fillEmail(email)
+                .fillPassword(validPassword)
+                .clickLogin();
+    }
+
+    @Test(description = "Unsuccessful login check")
+    public void LoginUnSuccesful() throws InterruptedException {
+        loginPage.fillEmail(email)
+                .fillPassword(invalidPassword)
+                .clickLogin();
+        loginPage.errorMessageController(errorLoginMessage);
+    }
+
+    @Test(description = "Registration control with existing e-mail")
+    public void ExistingRegistration() throws InterruptedException {
+        loginPage.fillRegisterMail(email);
+        loginPage.warningMessageController(errorExistingRegistration);
+    }
+
+    @Test(description = "Search and add to cart control")
+    public void SearchAndAddToCart() throws InterruptedException {
+        loginPage.fillEmail(email)
+                .fillPassword(validPassword)
+                .clickLogin();
+        loginPage.searchAndFill(searchData);
+    }
+}
